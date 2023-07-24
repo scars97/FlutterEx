9746eb26bf1fe2e76b92e7472772021774edeeb0
scars97 <qkr970504@gmail.com>
2023-07-24 23:22:48 +0900

    build project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Center(
+        child: Container( width: 50, height: 50, color: Colors.blue )
+      )
+          //Text('안녕')
+          //Icon(Icons.shop)
+          //Image.asset('1.jpg')
+          //Container( width: 50, height: 50, color: Colors.blue )
+    );
+
+  }
+}
